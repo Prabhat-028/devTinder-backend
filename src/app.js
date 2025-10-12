b1b4751b9eb01ffaf7bdd3318231bc1ff3a19b9d
Prabhat020 <kumar.prabhat23343@jeckukas.org.in>
2025-10-12 22:55:10 +0530

    learn the dynamic routing

--- src/app.js
+++ src/app.js
@@ -2,24 +2,13 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test",(req,res) => {
-    res.send("Hello from the server for test page");
+app.get("/main/:username/:userid", (req, res) => {
+    console.log(req.params);
+    res.send("successfully on main page");
+});
+app.post("/ma", (req, res) => {
+    res.send("dakad");
 })
-
-app.use("/hello", (req, res) => {
-  res.send("Hello from the server for hello page");
-});
-app.use("/uooo", (req, res) => {
-  res.send("Hello from the server fo ruooo page");
-});   
-app.use("/hd", (req, res) => {
-  res.send("Hello from the server for hd page");
-});
-
-app.use("/", (req, res) => {
-  res.send("Hello from the server for main page");
-});
-
 app.listen(1998, () => {
     console.log("server is successfully listening/running on port 1998");
 });
